d3de47180faafa1117997201954c8b2c9eb67843
thiago <thenrique2012@gmail.com>
2021-07-10 13:27:55 -0300

    resolução do exercicio 9 da lista 1 de haskell

--- haskell/lista-de-exercicios/lista-01/ex09.hs
+++ haskell/lista-de-exercicios/lista-01/ex09.hs
@@ -5,3 +5,23 @@
 -- Equilátero > Todos lados iguais
 -- Isósceles > Dois lados iguais
 -- Escaleno > Todos os lados diferentes
+
+triangleIsValid :: (Eq a, Num a, Ord a) => a -> a -> a -> Bool
+triangleIsValid a b c
+  | a < (b + c) = True
+  | b < (a + c) = True
+  | c < (a + b) = True
+  | otherwise = False
+
+triangleVariant :: (Eq a, Num a, Ord a) => a -> a -> a -> String
+triangleVariant a b c
+  | (a == b) && (a == c) && (b == c) = "Equilateral"
+  | a == b = "Isoceles"
+  | a == c = "Isoceles"
+  | b == c = "Isoceles"
+  | otherwise = "Scalene"
+
+triangleType :: (Eq a, Num a, Ord a) => a -> a -> a -> String
+triangleType a b c
+  | triangleIsValid a b c == False = "Invalído"
+  | otherwise = triangleVariant a b c
