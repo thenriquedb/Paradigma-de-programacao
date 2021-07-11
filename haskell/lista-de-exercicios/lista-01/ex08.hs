5d6033a637659c9137fc72e1f7b57b2c5c3a88f6
thiago <thenrique2012@gmail.com>
2021-07-11 11:01:34 -0300

    resolução do exercicio 8 da lista 1 de haskel

--- haskell/lista-de-exercicios/lista-01/ex08.hs
+++ haskell/lista-de-exercicios/lista-01/ex08.hs
@@ -1,2 +1,4 @@
 -- 8) Faça uma função recursiva para calcular a potência de dois. Por
--- exemplo, potDois 3 significa 23.
+-- exemplo, potDois 3 significa 2**3.
+potDois 0 = 1
+potDois n = 2 * potDois (n - 1)
